052dda42d70e975f68ee102066337630f052878d
Yichen Liu @ MAGNIF <yichenliu@arizona.edu>
2025-04-28 23:15:18 -0700

    added commit number into version

--- dingo/__init__.py
+++ dingo/__init__.py
@@ -1,2 +1,9 @@
 # imoprting structures:
 # grism -> utils -> galaxy -> kinematics
+
+# set up version
+try:
+    from setuptools_scm import get_version
+    __version__ = get_version(root='.', relative_to=__file__)
+except Exception:
+    __version__ = "unknown"
--- dingo/fitting.py
+++ dingo/fitting.py
@@ -1,3 +1,37 @@
+# TODO: logging
+
+import torch
+
+from . import kinematics
+
+def loss_kinematics_gradient(cutout_R, cutout_C, true_grism_R, true_grism_C, lambda_rest, x_G, y_G, velocity_params, dispersion_params_R, dispersion_params_C):
+    
+    # R grism
+
+    x_R, y_R, vz_R, iter_R = kinematics.iteratively_find_xy(
+        x_R, y_R, cutout_R, lambda_rest, x_G, y_G, 
+        **velocity_params, **dispersion_params_R
+    )
+
+    image_R = kinematics.bilinear_interpolte_intensity_torch(
+        x_R, y_R, true_grism_R, cutout_R
+    )
+
+    # C grism
+
+    x_C, y_C, vz_C, iter_C = kinematics.iteratively_find_xy(
+        x_C, y_C, cutout_C, lambda_rest, x_G, y_G, 
+        **velocity_params, **dispersion_params_C
+    )
+
+    image_C = kinematics.bilinear_interpolte_intensity_torch(
+        x_C, y_C, true_grism_C, cutout_C
+    )
+
+    # loss
+    loss = torch.sum((image_R - image_C)**2)
+
+    return loss
 
 
 
