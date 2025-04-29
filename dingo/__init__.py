4a87c3ce1969fe6f65e773ce1c40d9de6f5710ec
Yichen Liu @ MAGNIF <yichenliu@arizona.edu>
2025-04-28 23:26:51 -0700

    local_scheme=node seems depricated, use node-and-date

--- dingo/__init__.py
+++ dingo/__init__.py
@@ -4,6 +4,6 @@
 # set up version
 try:
     from setuptools_scm import get_version
-    __version__ = get_version(root='.', relative_to=__file__)
+    __version__ = get_version(root='..', relative_to=__file__)
 except Exception:
     __version__ = "unknown"
