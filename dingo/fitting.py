d6ff949bfed405a633098d6808c4eac67cdc1025
Yichen Liu @ MAGNIF <yichenliu@arizona.edu>
2025-05-02 21:15:41 -0700

    move kin fit to fitting.py and move kin plot to plot.py

--- dingo/fitting.py
+++ dingo/fitting.py
@@ -1,10 +1,32 @@
 # TODO: logging
 
 import torch
+import logging
 
 from . import kinematics
 
-def loss_kinematics_gradient(cutout_R, cutout_C, true_grism_R, true_grism_C, lambda_rest, x_G, y_G, velocity_params, dispersion_params_R, dispersion_params_C):
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+
+# Only add handler once 
+if not LOG.handlers:
+    console_handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+        datefmt='%H:%M:%S'  # Or '%Y-%m-%d %H:%M:%S' for full timestamp
+    )
+    console_handler.setFormatter(formatter)
+    LOG.addHandler(console_handler)
+
+
+#%% --------------------------------------------------------------------------
+# loss functions 
+# ----------------------------------------------------------------------------
+
+def loss_kinematics(
+    x_R, y_R, x_C, y_C, cutout_R, cutout_C, true_grism_R, true_grism_C, lambda_rest, 
+    x_G, y_G, velocity_params, dispersion_params_R, dispersion_params_C
+):
     
     # R grism
 
@@ -31,9 +53,53 @@ def loss_kinematics_gradient(cutout_R, cutout_C, true_grism_R, true_grism_C, lam
     # loss
     loss = torch.sum((image_R - image_C)**2)
 
-    return loss
+    return loss, x_R, y_R, x_C, y_C, iter_R, iter_C, image_R, image_C, vz_R, vz_C
 
 
+#%% --------------------------------------------------------------------------
+# fitting functions 
+# ----------------------------------------------------------------------------
 
-def fit_kinematics_gradient():
-    pass
+def fit_kinematics_gradient(
+        optimizer, scheduler, maxiter, cutout_R, cutout_C, true_grism_R, true_grism_C, 
+        lambda_rest, x_G, y_G, velocity_params, dispersion_params_R, dispersion_params_C
+):
+
+    x_R, y_R = kinematics.forward_dispersion_model(x_G, y_G, lambda_rest, **dispersion_params_R)
+    x_C, y_C = kinematics.forward_dispersion_model(x_G, y_G, lambda_rest, **dispersion_params_C)
+
+    losses = []
+    lrs = []
+    try: 
+        for i in range(maxiter):
+
+            optimizer.zero_grad()
+            
+            # NOTE: reusing x/y_R/C will accelerate finding xy
+            loss, x_R, y_R, x_C, y_C, iter_R, iter_C, image_R, image_C, vz_R, vz_C = loss_kinematics(
+                x_R, y_R, x_C, y_C, cutout_R, cutout_C, true_grism_R, true_grism_C, 
+                lambda_rest, x_G, y_G, velocity_params, dispersion_params_R, dispersion_params_C
+            )
+
+            if i==0:
+                LOG.info(f'Starting, loss={loss.item():.3f}, lr={optimizer.param_groups[0]['lr']:.5f}, maxiters finding xy={(iter_R, iter_C)}')
+            
+            if (i+1)%500==0:
+                LOG.info(f'Step {i+1}, loss={loss.item():.3f}, lr={optimizer.param_groups[0]['lr']:.5f}, maxiters finding xy={(iter_R, iter_C)}')
+
+            loss.backward()
+            optimizer.step()
+            scheduler.step(loss)
+            losses.append(loss.item())
+            lrs.append(optimizer.param_groups[0]['lr'])
+            
+            # TODO: apply data clamp
+            # theta_v.data.clamp_(min=0., max=torch.pi)
+            # inc_v.data.clamp_(min=0., max=torch.pi/2)
+
+            # break 
+    except Exception as e: 
+        print(repr(e))
+        pass
+
+    return losses, lrs, image_R, image_C, vz_R, vz_C
---
+++ dingo/plot.py
@@ -0,0 +1,70 @@
+# plot.py
+
+from astropy.stats import sigma_clipped_stats
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def asinhstretch(im):
+    _, _, std = sigma_clipped_stats(im)
+    return np.arcsinh(im / std)
+
+def plot_grism_result_2(true_im, model_im, vz, diff_im, title_prefix, axs_row, 
+                        velocity_params, dispersion_params):
+    dx = dispersion_params['dx'].detach().cpu().numpy()
+    dy = dispersion_params['dy'].detach().cpu().numpy()
+    x0_v = velocity_params['x0_v'].detach().cpu().numpy()
+    y0_v = velocity_params['y0_v'].detach().cpu().numpy()
+    true = true_im.detach().cpu().numpy()
+    model = model_im.detach().cpu().numpy()
+    residual = model - diff_im.detach().cpu().numpy()
+    vz = vz.detach().cpu().numpy()
+    resid_max = np.max(np.abs(residual))
+    vz_max = np.max(np.abs(vz))
+
+    im0 = axs_row[0].imshow(true)
+    axs_row[0].set_title(f'{title_prefix} Grism Image')
+    axs_row[0].grid(False)
+    axs_row[0].scatter(x0_v-dx, y0_v-dy, marker='o', s=100, c='none', edgecolor='lime')
+    plt.colorbar(im0, ax=axs_row[0], fraction=0.046, pad=0.04)
+
+    im1 = axs_row[1].imshow(model)
+    axs_row[1].set_title(f'{title_prefix} velocity corrected')
+    axs_row[1].grid(False)
+    axs_row[1].scatter(x0_v, y0_v, marker='o', s=100, c='none', edgecolor='lime')
+    plt.colorbar(im1, ax=axs_row[1], fraction=0.046, pad=0.04)
+
+    im2 = axs_row[2].imshow(residual, cmap='seismic', vmin=-resid_max, vmax=resid_max)
+    axs_row[2].set_title(f'{title_prefix} Residual')
+    axs_row[2].grid(False)
+    plt.colorbar(im2, ax=axs_row[2], fraction=0.046, pad=0.04)
+
+    im2 = axs_row[3].imshow(vz, cmap='seismic', vmin=-vz_max, vmax=vz_max)
+    axs_row[3].set_title(f'{title_prefix} velocity field')
+    axs_row[3].grid(False)
+    plt.colorbar(im2, ax=axs_row[3], fraction=0.046, pad=0.04)
+
+def plot_loss_and_lr(losses, lrs, steps=None):
+    if steps is None:
+        steps = range(len(losses))
+    
+    fig, ax1 = plt.subplots(figsize=(8,5))
+
+    color_loss = 'tab:blue'
+    ax1.plot(steps, losses, color=color_loss, label='Loss')
+    ax1.set_yscale('log')
+    ax1.set_xlabel('Step')
+    ax1.set_ylabel('Loss', color=color_loss)
+    ax1.tick_params(axis='y')
+
+    ax2 = ax1.twinx()  # create second y-axis sharing the same x-axis
+
+    color_lr = 'tab:red'
+    ax2.plot(steps, lrs, color=color_lr, linestyle='--', label='Learning Rate')
+    ax2.set_yscale('log')
+    ax2.set_ylabel('Learning Rate', color=color_lr)
+    ax2.tick_params(axis='y')
+
+    fig.tight_layout()
+    plt.title('Loss and Learning Rate Evolution')
+    plt.show()
